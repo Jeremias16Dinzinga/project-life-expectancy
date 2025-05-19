dc95ad966513e4726133e90863957ffe9b5d897f
Jeremias Dinzinga <117875623+Jeremias16Dinzinga@users.noreply.github.com>
2025-05-19 10:16:28 +0100

    generalStatistics

--- Class_dataset.py
+++ Class_dataset.py
@@ -1,33 +1,14 @@
-
 import pandas as pd
-
 
 class CDataSet:
     def __init__(self):
         self.nome_ficheiro = "ORIGINAL/Life Expectancy Data.csv"
-        self.__df = pd.read_csv(
+        self._df = pd.read_csv(
             self.nome_ficheiro, sep=","
         )
 
-    def dimensao(self):
-        return {"linhas": self.__df.shape[0],
-                "colunas": self.__df.shape[1]
-                }
-
-    def info(self):
-        print(self.__df.info)
-
-    def descricao(self):
-        print(self.__df.describe().to_string())
-
-    def ver(self):
-        print(self.__df)
-
-
-d = CDataSet()
-x = d.dimensao()
-
-print (x)
-# d.ver()
-# d.info()
-d.descricao()
+    def estatisticas_gerais(self):
+        print("\nResumo Estatístico:")
+        print(self._df.describe())
+        print("\nInformações:")
+        print(self._df.info())
--- Programa_Principal.py
+++ Programa_Principal.py
@@ -1,5 +1,43 @@
 
 from Class_dataset import *
-d.dimensao()
+
+d = CDataSet()
+
+def menu():
+    while True:
+        print("\n===== MENU DE ANÁLISE =====")
+        print("1. Estatísticas gerais")
+        print("2. Expectativa de vida por país")
+        print("3. Expectativa de vida ao longo dos anos")
+        print("4. Comparar países desenvolvidos vs. em desenvolvimento")
+        print("5. Mostrar correlação entre variáveis")
+        print("6. Países com maior/menor expectativa de vida em um ano")
+        print("7. Sair")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == '1':
+            d.estatisticas_gerais()
+        elif opcao == '2':
+            print("Processando...")
+            #d.expectativa_por_pais()
+        elif opcao == '3':
+            print("Processando...")
+            #expectativa_ao_longo_dos_anos()
+        elif opcao == '4':
+            print("Processando...")
+            #comparacao_status()
+        elif opcao == '5':
+            print("Processando...")
+            #correlacoes()
+        elif opcao == '6':
+            print("Processando...")
+            #top_paises()
+        elif opcao == '7':
+            print("Encerrando o programa. Até logo!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+menu()
 
 
